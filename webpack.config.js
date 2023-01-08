b940cbf2327b9b616c89a0770bb5fd97fba38e29
Rommel B. Mabini Jr <rommelbrillantesmabinijr@gmail.com>
2023-01-08 11:36:20 +0800

    Add bundled resources with original resource files

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
+        
         rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
